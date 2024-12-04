8ea253a76b5800cf8e8e37831257c9f26b82ee82
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2024-12-05 02:19:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Select the button
+const exploreButton = document.getElementById('exploreButton');
+
+// Add a click event listener
+exploreButton.addEventListener('click', () => {
+    // Redirect to signIn.html
+    window.location.href = '/pages/signInPage.html';
+});
